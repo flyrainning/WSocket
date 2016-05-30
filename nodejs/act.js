05890fc660792f6cfc534f8ceb1e35d9d3622ea9
flyrainning <flyrainning@gmail.com>
2016-05-30 13:16:42 +0800

    Create act.js

---
+++ nodejs/act.js
@@ -0,0 +1,24 @@
+exports.get_a_json=function(data,ws){
+	var res={};
+	res.test='test';
+
+	return res;
+}
+
+
+exports.get_a_string=function(data,ws){
+	var res="test";
+
+	return res;
+}
+
+
+
+exports.get_a_binery=function(data,ws){
+	var res = new Float32Array(5);
+
+	return res;
+}
+
+
+	
